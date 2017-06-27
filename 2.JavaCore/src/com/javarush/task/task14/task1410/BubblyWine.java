543a59f134a7f7886ac9cd378137efddc58a78ed
tolik22 <tolikt22@gmail.com>
2017-06-27 22:01:02 +0300

    Wine

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1410/BubblyWine.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task14.task1410;
+
+/**
+ * Created by tolik on 21.06.2017.
+ */
+public class BubblyWine extends Wine {
+    @Override
+    public String getHolidayName() {
+        return "Новый Год";
+    }
+}
