f05ab5f395ea144b18229bb0393a0533f976768b
tolik22 <tolikt22@gmail.com>
2017-06-28 17:46:21 +0300

    MovieFactory

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1413/CompItem.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task14.task1413;
+
+/**
+ * Created by tolik on 28.06.2017.
+ */
+public interface CompItem {
+    String getName();
+}
---
+++ 2.JavaCore/src/com/javarush/task/task14/task1413/Mouse.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task14.task1413;
+
+/**
+ * Created by tolik on 28.06.2017.
+ */
+public class Mouse implements CompItem {
+    @Override
+    public String getName() {
+        return "Mouse";
+    }
+}
