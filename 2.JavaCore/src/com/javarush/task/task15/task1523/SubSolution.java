fe12898204fbecbc9e6efb30f7a061435a91ab88
tolik22 <tolikt22@gmail.com>
2017-07-09 21:51:15 +0300

    Reading file in static block

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1523;
+
+/**
+ * Created by tolik on 09.07.2017.
+ */
+public class SubSolution extends Solution {
+    SubSolution() {
+    }
+    protected SubSolution(int i) {
+    }
+    public SubSolution(String s) {
+    }
+
+
+}
