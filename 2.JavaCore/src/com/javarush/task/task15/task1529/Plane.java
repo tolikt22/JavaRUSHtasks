590a3dbdf5a108433f720b2386c1c6abbbb9c146
tolik22 <tolikt22@gmail.com>
2017-07-21 22:46:18 +0300

    My first & second Threads

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Plane.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task15.task1529;
+
+/**
+ * Created by tolik on 19.07.2017.
+ */
+public class Plane implements Flyable {
+//    private int passengers;
+
+    public Plane(int passengers) {
+//        this.passengers = passengers;
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
