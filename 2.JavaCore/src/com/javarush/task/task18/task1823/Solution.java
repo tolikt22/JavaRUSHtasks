8cbbab63fe6e1ad1b44b99d4dc75ecf0c1ddc6dd
tolik22 <tolikt22@gmail.com>
2017-09-23 16:11:48 +0300

    Нити и байты

--- 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
@@ -1,23 +1,68 @@
 package com.javarush.task.task18.task1823;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
-/* 
+/*
 Нити и байты
 */
 
 public class Solution {
     public static Map<String, Integer> resultMap = new HashMap<String, Integer>();
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName;
+        while (!(fileName = reader.readLine()).equals("exit")) {
+            new ReadThread(fileName).start();
+        }
     }
 
     public static class ReadThread extends Thread {
-        public ReadThread(String fileName) {
-            //implement constructor body
+        private String fileName;
+        FileInputStream fis;
+
+        public ReadThread(String fileName) throws FileNotFoundException {
+            this.fileName = fileName;
+            fis = new FileInputStream(this.fileName);
         }
         // implement file reading here - реализуйте чтение из файла тут
+
+        @Override
+        public void run() {
+            try {
+                List<Integer> bytes = new ArrayList<>();
+                while (fis.available() > 0) bytes.add(fis.read());
+//                System.out.println(Arrays.toString(bytes.toArray()));
+                findMaxByte(bytes);
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void findMaxByte(List<Integer> bytes) {
+            int tmp, count = 0, maxRepeat = 0, maxByte = 0;
+            for (int i = 0; i < bytes.size(); i++) {
+                tmp = bytes.get(i);
+                for (int j = 0; j < bytes.size(); j++) {
+                    if (tmp == bytes.get(j)) count++;
+                }
+                if (maxRepeat < count) {
+                    maxRepeat = count;
+                    maxByte = tmp;
+                }
+                count = 0;
+            }
+            synchronized (resultMap) {
+                resultMap.put(this.fileName, maxByte);
+                System.out.println(maxByte);
+            }
+//            System.out.println(Arrays.toString(resultMap.entrySet().toArray()));
+
+//            for (Map.Entry<String,Integer> pair : resultMap.entrySet()){
+//                System.out.println(pair.getKey());
+//            }
+        }
     }
 }
