d437d37cfb1fe7a3a6b75314dc95bd14cc91b5bc
tolikt22 <tolikt22@gmail.com>
2017-10-15 22:51:54 +0300

    Дублируем текст

--- 2.JavaCore/src/com/javarush/task/task19/task1915/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1915/Solution.java
@@ -4,10 +4,27 @@ package com.javarush.task.task19.task1915;
 Дублируем текст
 */
 
+import java.io.*;
+
 public class Solution {
     public static TestString testString = new TestString();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = new String(reader.readLine());
+        reader.close();
+        PrintStream console = System.out;
+        FileOutputStream fos = new FileOutputStream(fileName);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(baos);
+        System.setOut(stream);
+
+        testString.printSomething();
+        String string = baos.toString();
+        fos.write(string.getBytes());
+        fos.close();
+        System.setOut(console);
+        System.out.println(string);
     }
 
     public static class TestString {
