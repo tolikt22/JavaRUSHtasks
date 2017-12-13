5fb2e6911eea80be62328cd4b4487b23d4bd3177
tolikt22 <tolikt22@gmail.com>
2017-12-13 23:26:12 +0200

    Кроссворд 20207 в процессе

--- 2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
@@ -17,8 +17,9 @@ public class Solution {
                 {'p', 'o', 'e', 'e', 'j', 'j'}
         };
 //        detectAllWords(crossword, "home", "same");
-//        System.out.println(Arrays.toString(detectAllWords(crossword, "home", "same", "kerpo","fsgrj","plgml","eroo","hrrp").toArray()));
-        System.out.println(Arrays.toString(detectAllWords(crossword, "e").toArray()));
+        System.out.println(Arrays.toString(detectAllWords(crossword, "home", "same", "kerpo", "fsgrj", "plgml", "prek", "hrrp", "e").toArray()));
+//        System.out.println(Arrays.toString(detectAllWords(crossword, "ngrov").toArray()));
+//        System.out.println(Arrays.toString(detectAllWords(crossword, "lngrov").toArray()));
         /*
 Ожидаемый результат
 home - (5, 3) - (2, 0)
@@ -30,82 +31,79 @@ same - (1, 1) - (4, 1)
         String[] w = words;                                  // список слов в стринговый массив
         Word oneWord;
         List<Word> wordList = new ArrayList<>();
-        for (int i = 0; i < w.length; i++) {                 //перебираем по слову
-            String word = w[i];
-            for (int j = 1; j <= 8; j++) {                   // in 8 directions
-                oneWord = findWord(crossword, word, j);
-                if (oneWord != null) wordList.add(oneWord);
+        for (int k = 0; k < w.length; k++) {                 //перебираем по слову
+            String word = w[k];
+            char[] chars = word.toCharArray();                                         // parsing String word to char[]
+            for (int i = 0; i < crossword.length; i++) {
+                for (int j = 0; j < crossword[i].length; j++) {
+                    if (crossword[i][j] == chars[0]) {              //если первая буква слова совпадает c координатой массива
+                        oneWord = detectDirections(j, i, chars, crossword);//отправляем на проверку по 8 направл.
+                        if (oneWord != null && word.length() != 1) {
+                            wordList.add(oneWord);
+                        }
+                    }
+                }
             }
-
         }
         return wordList;
     }
 
-    private static Word findWord(int[][] crossword, String w, int direction) { //1 word in 1 direction
-        char[] word = w.toCharArray();                                         // parsing String word to char[]
-        Word oneWord = null;
-
-
-        for (int i = 0; i < crossword.length; i++) {
-            for (int j = 0; j < crossword[i].length; j++) {
-                if (crossword[i][j] == word[0]) {              //если первая буква слова совпадает c координатой массива
-                    oneWord = detectDirections(j, i, word, crossword, direction);//отправляем на проверку по 1 направлению
-                }
-            }
-        }
-        return oneWord;  // возвращаем найденное слово или null
-    }
-
-    private static Word detectDirections(int x, int y, char[] word, int[][] crossword, int direction) {//ищем слово в 1 напр.
+    private static Word detectDirections(int x, int y, char[] word, int[][] crossword) {//ищем слово во всех напр.
         StringBuilder wordString = new StringBuilder();
         Word foundWord = null;
-        int startX = x, startY = y;
-        int endX, endY;
-        int count = 0;   //matching chars counter
-        int charCount = 0; //in word chars counter
-        while (y >= 0 && x >= 0 && y < crossword.length && x < crossword[0].length && charCount < word.length) {    //keeping array borders
-            if (word[charCount] == crossword[y][x]) { // !!
-                wordString.append(word[charCount]);
-                count++;
-            }
-            if (count == word.length) {
-                endX = x;
-                endY = y;
-                foundWord = new Word(wordString.toString());
-                foundWord.setStartPoint(startX, startY);
-                foundWord.setEndPoint(endX, endY);
-            }
-            charCount++;
+        int tmpX = x, tmpY = y;
+        int startX = tmpX, startY = tmpY;
 
-            switch (direction) {  //moving in the direction indicated
-                case 1:
-                    x++;
-                    break;
-                case 2:
-                    x++;
-                    y--;
-                    break;
-                case 3:
-                    y--;
-                    break;
-                case 4:
-                    y--;
-                    x--;
-                    break;
-                case 5:
-                    x--;
-                    break;
-                case 6:
-                    x--;
-                    y++;
-                    break;
-                case 7:
-                    y++;
-                    break;
-                case 8:
-                    y++;
-                    x++;
-                    break;
+        for (int direction = 1; direction <= 8; direction++) {
+            int endX, endY;
+            int count = 0;   //matching chars counter
+            int charCount = 0; //in word chars counter
+            x=tmpX;y= tmpY;
+            while (y >= 0 && x >= 0 && y < crossword.length && x < crossword[0].length && charCount < word.length) {    //keeping array borders
+                if (word[charCount] == crossword[y][x]) { // !!
+                    wordString.append(word[charCount]);
+                    count++;
+                }
+                if (count == word.length) {
+                    endX = x;
+                    endY = y;
+                    foundWord = new Word(wordString.toString());
+                    foundWord.setStartPoint(startX, startY);
+                    foundWord.setEndPoint(endX, endY);
+//                    return foundWord;
+                }
+                charCount++;
+
+                switch (direction) {  //moving in the direction indicated
+                    case 1:
+                        x++;
+                        break;
+                    case 2:
+                        x++;
+                        y--;
+                        break;
+                    case 3:
+                        y--;
+                        break;
+                    case 4:
+                        y--;
+                        x--;
+                        break;
+                    case 5:
+                        x--;
+                        break;
+                    case 6:
+                        x--;
+                        y++;
+                        break;
+                    case 7:
+                        y++;
+                        break;
+                    case 8:
+                        y++;
+                        x++;
+                        break;
+                }
             }
         }
         return foundWord;
