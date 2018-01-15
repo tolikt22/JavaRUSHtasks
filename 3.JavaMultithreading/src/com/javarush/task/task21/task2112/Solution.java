d5be615834973ad1141ba96b39971ee2caef6710
tolikt22 <tolikt22@gmail.com>
2018-01-15 23:16:49 +0200

    AutoCloseable и try-with-resources

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2112/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task21.task2112;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        DBConnectionManager dbConnectionManager = new DBConnectionManager();
+        try (FakeConnection fakeConnection = dbConnectionManager.getFakeConnection()) {
+            System.out.println("Entering the body of try block.");
+            fakeConnection.usefulOperation();
+            fakeConnection.unsupportedOperation();
+        }
+    }
+}
